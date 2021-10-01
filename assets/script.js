5c93e9a5777dcb8c20056d01b82fb335b57d0751
SpecsNStats <specsnstats@gmail.com>
2021-09-30 21:40:05 -0600

    api successfully fetching

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,27 @@
 // create a variable that pulls a specific city based on clicking a city button or clicking search in the search bar
-    // create a button listener for each button
-    // in the button listener, have the function run that saves the value of the button pressed (or the value of the text for the search button) in a variable
-    //  
-
-// create a variable for the api
+// create a button listener for each button
+// create a variable for the button element and the search button and the text value of the search button
+var btnEL = $("button");
+// in the button listener, have the function run that saves the value of the button pressed (or the value of the text for the search button) in a variable
+btnEL.on("click", getApi)
 
 // retrieve information from the api
-
+function getApi(weatherSource) {
+  var btnEL = $("button");
+  var searchBtnEl = $("#search-btn");
+  var searchBtnText = $("#text-box").val();
+  var APIKey = "eef440075f231dabd98329edc16d0dae";
+  var city = "boston";
+  var weatherSource = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+  fetch(weatherSource)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
 // create a then that retrieves the individual information for day 1
-
-
 
 // WHEN I search for a city
 // THEN I am presented with current and future conditions for that city and that city is added to the search history
