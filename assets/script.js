dc86f653fb08215cb77bb5fbc56b4f0c12832b82
SpecsNStats <specsnstats@gmail.com>
2021-10-01 00:18:31 -0600

    date working, icon working

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,52 @@
-// create a variable that pulls a specific city based on clicking a city button or clicking search in the search bar
-// create a button listener for each button
-// create a variable for the button element and the search button and the text value of the search button
-var btnEL = $("button");
-// in the button listener, have the function run that saves the value of the button pressed (or the value of the text for the search button) in a variable
-btnEL.on("click", getApi)
+var searchBtnEl = $("#search-btn");
+var date = moment().format('L');
+console.log(date);
+var cityNameEl = $("#city-name")
+searchBtnEl.on("click", getApi);
+
 
 // retrieve information from the api
-function getApi(weatherSource) {
-  var btnEL = $("button");
-  var searchBtnEl = $("#search-btn");
-  var searchBtnText = $("#text-box").val();
-  var APIKey = "eef440075f231dabd98329edc16d0dae";
-  var city = "boston";
-  var weatherSource = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-  fetch(weatherSource)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-    });
+function getApi(coordsSource) {
+    var searchBtnText = $("#text-box");
+    var APIKey = "eef440075f231dabd98329edc16d0dae";
+    var city = searchBtnText.val();
+    console.log(city);
+    var coordsSource = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+// fetch the location info
+    fetch(coordsSource)
+        .then(function (response) {
+        return response.json();
+        })
+        .then(function (data) {
+// grab the coordinates of the city and save them as variables
+        var lat = data.coord.lat
+        console.log(lat);
+        var lon = data.coord.lon
+        console.log(lon);
+// use those variables to then generate the appropriate api path for the daily/week forecast
+        var finalAPI = "https://api.openweathermap.org/data/2.5/onecall?lat="+ lat + "&lon=" + lon + "&appid=" + APIKey
+// fetch the locations info again, this time using the coords into the better api
+        fetch(finalAPI)
+            .then(function (response) {
+            return response.json();
+            })
+            .then(function (data) {
+            console.log(data);
+// start doing stuff with the info
+    // create weather icon var from API info
+            var icon = "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png"
+            console.log(icon);
+    // create link to weather icon element in html
+            var cityNameIconEl = $("#city-name-icon")
+    // update weather icon src to new icon from API info
+            cityNameIconEl.attr("src", icon)
+    // create desired var for new city title
+            var newCityName = city + "(" + date + ")"
+            cityNameEl.innerHTML = newCityName
+            })
+        });
+         
+    
 }
 // create a then that retrieves the individual information for day 1
 
